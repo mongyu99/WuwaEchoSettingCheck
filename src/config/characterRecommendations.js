573f33cc881a3aa4f0120e95ccb277f7e0fcc0a1
Mongyu <Mongyu249@gmail.com>
2026-08-19 23:54:44 +0900

    - Add Chara Recommned Stat

--- src/config/characterRecommendations.js
+++ src/config/characterRecommendations.js
@@ -27,15 +27,15 @@ export const CHARACTER_RECOMMENDATIONS = {
   ciaccona: { weapon: '숲속의 아리아[전무], 부동의 안개[상시], 화려한 악곡[4성]', echoSet: '끝없는 하늘[43311]', echoMainStat: [{ cost: '43111', stats: '크확 / 기류피증 / 공퍼'}], critRatio: '크확 75% / 크피 275%', resonanceEfficiency: '120~', atk: '2100[속공] 1800[속속]', energyCost: '125', notes: ['카르티시아 전용 서포터로 사용'] },
   cartethyia: { weapon: '숙명에 맞서는 관 [전무], 날카로운 봄[카멜]', echoSet: '영광이 깃든 바람[44111]', echoMainStat: [{ cost: '43111', stats: '크확 / 1코스트 체% / 3코스트 피증'}], critRatio: '크확 70% / 크피 260%', resonanceEfficiency: '120', atk: '50000[44111]', energyCost: '120', notes: ['샤콘과 기류 방랑자 파티 사용\n 43111은 카멜 전무용임 전무는 44111'] },
   rover_aero: { weapon: '혈맹의 약속[전무], 천년의 회류[상시], 행진의 서곡[4성]', echoSet: '영광이 깃든 바람[43311] / 찬란한 광휘[43311]', echoMainStat: [{ cost: '43111', stats: '크확크피 / 기류피증 / 공퍼공효'}], critRatio: '크확 60% / 크피 200%', resonanceEfficiency: '150[피증+공%] 200[피증+공효]', atk: '1900', energyCost: '150', notes: ['찬란한 광휘 사용시 기존 힐러 세팅 그대로'] },
-  iuno: { weapon: '세상 만물의 진리[전무], 팔방의 천추[상리요], 불빛의 심판[젠니], 물결의 파동[상시]', echoSet: '영광의 칼날로 만들어진 왕관[3세트(크피20%)] / 스쳐가는 바람[2세트] or 끝없는 하늘[2세트] or 영광이 깃든 바람[2세트] / 떠오르는 구름[43311]', echoMainStat: '크피 기류피증 기류피증 기류피증 공퍼', critRatio: '크확 80% / 크피 260%', resonanceEfficiency: '130~', atk: '2300[속공] 2000[속속]', energyCost: '125', notes: [] },
-  qiuyuan: { weapon: '', echoSet: '', echoMainStat: '', critRatio: '', resonanceEfficiency: '', atk: '', energyCost: '', notes: [] },
-  sigrika: { weapon: '', echoSet: '', echoMainStat: '', critRatio: '', resonanceEfficiency: '', atk: '', energyCost: '', notes: [] },
+  iuno: { weapon: '세상 만물의 진리[전무], 팔방의 천추[상리요], 불빛의 심판[젠니], 물결의 파동[상시]', echoSet: '영광의 칼날로 만들어진 왕관[3세트(크피20%)] 스쳐가는 바람[2세트] or 끝없는 하늘[2세트] or 영광이 깃든 바람[2세트] / 떠오르는 구름[43311]', echoMainStat: '크피 기류피증/기류피증 기류피증/공퍼', critRatio: '크확 80% / 크피 260%', resonanceEfficiency: '130~', atk: '2300[속공] 2000[속속]', energyCost: '125', notes: [] },
+  qiuyuan: { weapon: '푸른 의지[전무], 날카로운 봄[카멜리아], 솟아오르는 화염[장리], 천년의 회류[상시]', echoSet: '만물의 숨결에 비롯된 울림[3세트] 스쳐가는 바람[2세트] or 끝없는 하늘[2세트] or 영광이 깃든 바람[2세트] / 떠오르는 구름[43311]', echoMainStat: '크피 기류피증/기류피증 기류피증/공퍼', critRatio: '크확 70% / 크피 260%', resonanceEfficiency: '130~', atk: '2350[속공] 2050[속속]', energyCost: '125', notes: ['떠오르는 구름 세트 사용시 파티딜이 더 높고 공명 효율 요구치 10% 감소'] },
+  sigrika: { weapon: '솔스원의 해석[전무], 불빛의 심판[젠니], 한낮의 의지[루크]', echoSet: '함의의 소리를 따라[43311]', echoMainStat: '크확 공효/공퍼 공퍼/공퍼', critRatio: '크확 70% / 크피 270%', resonanceEfficiency: '150', atk: '260', energyCost: '125', notes: ['부옵으로 공효 35% 이상 챙길 수 있으면 3코 공퍼 2개 채용 가능하다고 함 파수인 2돌이면 속성 피증, 공퍼가 우위 그리고 공명 스킬도 찍어줄 수 있으면 찍어주셈'] },
   // 기류
 
   // 용융
-  encore: { weapon: '', echoSet: '', echoMainStat: '', critRatio: '', resonanceEfficiency: '', atk: '', energyCost: '', notes: [] },
-  changli: { weapon: '', echoSet: '', echoMainStat: '', critRatio: '', resonanceEfficiency: '', atk: '', energyCost: '', notes: [] },
-  brant: { weapon: '', echoSet: '', echoMainStat: '', critRatio: '', resonanceEfficiency: '', atk: '', energyCost: '', notes: [] },
+  encore: { weapon: '꼭두각시의 손, 옥수 비단, 파도의 기록, 청음', echoSet: '솟구치는 용암[43311]', echoMainStat: '크확/크피 용융피증/용융피증 용융피증/공퍼', critRatio: '크확 70% / 크피 270%', resonanceEfficiency: '120', atk: '1900', energyCost: '125', notes: ['2체인에 공명 에너지 회복이 달려있어서 2체인 이상이면 공효 요구치가 낮아짐'] },
+  changli: { weapon: '솟아오르는 화염[전무], 천년의 회류[상시], 야귀의 신념[튜닝]', echoSet: '솟구치는 용암[43311]', echoMainStat: '크확/크피 용융피증/공퍼 공퍼/공퍼', critRatio: '크확 70% / 크피 260%', resonanceEfficiency: '120', atk: '2500', energyCost: '125', notes: ['2체인에 이화 획득시 크리 25퍼 증가가 있어서 2체인 이상이면 4코 주옵 크리피해 고려'] },
+  brant: { weapon: '흔들리지 않는 용기[전무], 날카로운 봄[카멜리아],천년의 회류[상시], 행진의 서곡[4성]', echoSet: '파도에 맞선 용기[43311]', echoMainStat: '크확 공명효율/공명효율 공명효율/용융피증', critRatio: '크확 75% / 크피 220%', resonanceEfficiency: '270', atk: '1700[패시브X]', energyCost: '175', notes: ['일공피 >> 공% / 용융속성 전용 서포터로 사용하거나, 일반공격 피해 버프를 주는 아군과 사용 최대 공효 280 / 2체인 이상 크확 55 크피 260'] },
   lupa: { weapon: '', echoSet: '', echoMainStat: '', critRatio: '', resonanceEfficiency: '', atk: '', energyCost: '', notes: [] },
   galbrena: { weapon: '', echoSet: '', echoMainStat: '', critRatio: '', resonanceEfficiency: '', atk: '', energyCost: '', notes: [] },
   mornye: { weapon: '', echoSet: '', echoMainStat: '', critRatio: '', resonanceEfficiency: '', atk: '', energyCost: '', notes: [] },
